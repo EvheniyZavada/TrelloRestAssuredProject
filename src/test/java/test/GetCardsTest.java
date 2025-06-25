c4e3b1914382af48206c9eb1caa9e01d39a740d4
EvheniyZavada <zavada1997@gmail.com>
2025-06-25 16:17:31 +0300

    2

--- src/test/java/test/GetCardsTest.java
+++ src/test/java/test/GetCardsTest.java
@@ -1,6 +1,8 @@
 package test;
 
 import io.restassured.RestAssured;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.junit.jupiter.api.Test;
 
 import java.util.Map;
@@ -9,6 +11,19 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.lessThan;
 
 public class GetCardsTest {
+    private static final Log log = LogFactory.getLog(GetCardsTest.class);
+    @Test
+    public void checkGetLists(){
+        RestAssured.given()
+                .log().method()
+                .baseUri("https://api.trello.com/1")
+                .queryParams(Map.of("key", TrelloAuthInfo.getKey(),"token", TrelloAuthInfo.getToken()))
+                .pathParam("id", "68499967310c9b0128bb22c1")
+                .get("/boards/{id}/lists")
+                .then()
+                .statusCode(200)
+                .log().body();
+    }
 
     @Test
     public void checkGetCards(){
@@ -16,12 +31,13 @@ public class GetCardsTest {
                 .log().method()
                 .baseUri("https://api.trello.com/1")
                 .queryParams(Map.of("key", TrelloAuthInfo.getKey(),"token", TrelloAuthInfo.getToken()))
-                .pathParam("id", "68345f96a7da9a0cf7c72917")
+                .pathParam("id", "68499967310c9b0128bb2315")
                 .get("/lists/{id}/cards")
                 .then()
                 .statusCode(200)
                 .log().all();
     }
+
     @Test
     public void checkGetCard(){
         RestAssured.given()
