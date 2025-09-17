d0cfdc0351d0d62d8a8ce50f69262c281bf642b0
Александр Ульяницкий <alex.backend.dev@gmail.com>
2025-09-17 15:18:44 +0300

    Fixed JUnit dependencies

--- src/test/java/test/TestRunner.java
+++ src/test/java/test/TestRunner.java
@@ -1,8 +1,11 @@
 package test;
 
-import org.junit.platform.suite.api.*;
 
-import static io.cucumber.core.options.Constants.FILTER_TAGS_PROPERTY_NAME;
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.ConfigurationParameters;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
 import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 
 
